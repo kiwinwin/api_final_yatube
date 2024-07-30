6d26aed1602fdf654c783d1776d242970c627a33
kiwinwin <galia.baibulova98@gmail.com>
2024-07-30 12:44:17 +0300

    v_1_0_3

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
+    
     'posts.apps.PostsConfig',
     'api.apps.ApiConfig',
 ]
